09cbe2a74e940796d4707b67b0be3b3c11bfc087
vineethk123 <vineeth.kolluru@zemosolabs.com>
2019-01-02 14:53:49 +0530

    Add HTML formatting

--- src/main/java/com/example/assignment/controller/BookController.java
+++ src/main/java/com/example/assignment/controller/BookController.java
@@ -5,6 +5,7 @@ import com.example.assignment.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @RestController
@@ -13,8 +14,14 @@ public class BookController {
     BookRepository bookRepository;
 
     @GetMapping("/books/{id}")
-    public Optional<Book> getBook(@PathVariable Long id) {
-        return bookRepository.findById(id);
+    public String getBook(@PathVariable Long id) {
+        Optional<Book> op = bookRepository.findById(id);
+        if (op.isPresent()) {
+            Book book = op.get();
+            return "<p>Id: " + book.getId() + "</p><p>Author: " + book.getAuthor() +
+                    "</p><p>Title: " + book.getTitle() + "</p>";
+        }
+        return "";
     }
 
     @PostMapping("/books")
